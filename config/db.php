92bfc068a8d98ba8588862c826d428bcf50cc85b
Jonathan Morales Salazar <blonder413@gmail.com>
2016-10-20 10:48:50 -0500

    migraciones

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=blogyii2',
+    'username' => 'pruebas',
+    'password' => '123456',
+    'charset' => 'utf8',
+];
